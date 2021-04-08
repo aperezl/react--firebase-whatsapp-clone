f5593ad2b3e526b50ad62517bc0a8dff62c64177
Antonio Manuel Perez Lopez <aperezl@gmail.com>
2021-04-09 00:34:40 +0200

    add conditions to add email

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,17 +4,25 @@ import ChatIcon from '@material-ui/icons/Chat'
 import MoreVertIcon from '@material-ui/icons/MoreVert'
 import SearchIcon from '@material-ui/icons/Search'
 import * as EmailValidator from 'email-validator'
-import { auth } from '../firebase'
+import { auth, db } from '../firebase'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollection } from 'react-firebase-hooks/firestore'
 
 function Sidebar() {
+  const [user] = useAuthState(auth)
+  const userChatRef = db.collection('chats').where('users', 'array-contains', user.email)
+  const [chatsSnapshot] = useCollection(userChatRef)
+
+  const chatAlreadyExists = recipientEmail => !!chatsSnapshot?.docs.find(chat => chat.data().users.find(user => user === recipientEmail)?.length > 0)
 
   const createChat = () => {
     const input = prompt('Please enter email')
     if (!input) return null
-    if (EmailValidator.validate(input)) {
-
+    if (EmailValidator.validate(input) && !chatAlreadyExists(input) && input !== user.email) {
+      db.collection('chats').add({
+        users: [user.email, input]
+      })
     }
-
   }
   return (
     <Container>
